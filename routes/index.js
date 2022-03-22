e877c93bc771ab6eee6b794e16636a12a5eb223c
FAREAST\aseemgoyal <aseemgoyal@microsoft.com>
2022-03-22 14:28:09 +0530

    Integrate with Azure cognitive search, SUCCESSFUL WORKING!!

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
+const { SearchIndexClient, SearchClient, AzureKeyCredential, odata } = require("@azure/search-documents");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async (req, res, next) => {
+  try{
+      // Load the .env file if it exists
+    require("dotenv").config();
+
+    // Getting endpoint and apiKey from .env file
+    const endpoint = process.env.SEARCH_API_ENDPOINT || "";
+    const apiKey = process.env.SEARCH_API_KEY || "";
+    const indexName = "fhldocumentsearch-index";
+    const searchClient = new SearchClient(endpoint, indexName, new AzureKeyCredential(apiKey));
+    await sendQueries(searchClient, "azure");
+    res.render('index', { title: 'Express' });
+  }
+  catch (err) {
+    console.error(err);
+    next(err);
+  }
 });
+
+
+ async function sendQueries(searchClient, text) {
+  console.log('Query #1 - search everything:');
+  let searchOptions = {
+      includeTotalCount: true,
+  };
+
+  let searchResults = await searchClient.search(text, searchOptions);
+  for await (const result of searchResults.results) {
+      //console.log(`${JSON.stringify(result.document)}`);
+  }
+  console.log(`Result count: ${searchResults.count}`);
+
+  // remaining queries go here
+}
+
 
 module.exports = router;
