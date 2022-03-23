c029814f9c6a06919a377629254c668ba55994be
FAREAST\aseemgoyal <aseemgoyal@microsoft.com>
2022-03-23 13:58:25 +0530

    Support url from the cognitive search as well

--- routes/index.js
+++ routes/index.js
@@ -35,8 +35,10 @@ router.get('/', async (req, res, next) => {
   let searchResults = await searchClient.search(text, searchOptions);
   for await (const result of searchResults.results) {
       //console.log(`${JSON.stringify(result.document)}`);
-
-        documents.push(result.document.content);
+      const documentObjectCognitiveSearch = new Object();
+      documentObjectCognitiveSearch.content = result.document.content;
+      documentObjectCognitiveSearch.url = enrichURL(getBase64Decode(result.document.metadata_storage_path));
+      documents.push(documentObjectCognitiveSearch);
         //console.log(`\n\n\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`);
 
         //console.log(`${JSON.stringify(result)}`);
@@ -54,12 +56,19 @@ router.get('/', async (req, res, next) => {
     //console.log(`${JSON.stringify(documents[i])}`);
 
     console.log(`\n\n\n!!!!!!!!!!!!!!!!!!            SUMMMARY   START          !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`);
-    var summaryResult = await queryBART({"inputs": JSON.stringify(documents[i])});
-    console.log(JSON.stringify(summaryResult[0].summary_text));
-    summary.push(summaryResult[0].summary_text);
+    var summaryResult = await queryBART({"inputs": JSON.stringify(documents[i].content)});
+    //console.log(JSON.stringify(summaryResult[0].summary_text));
+    const documentSummaryObject = new Object();
+    documentSummaryObject.content = summaryResult;
+    documentSummaryObject.url = documents[i].url;
+
+    summary.push(documentSummaryObject);
+    console.log(`Summary: ${documentSummaryObject.url}`);
+    console.log(`Summary: ${JSON.stringify(documentSummaryObject).replace("\\r", "")}`);
+
     console.log(`\n\n\n!!!!!!!!!!!!!!!!!!            SUMMMARY  END           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`);
   }
-  //console.log(`Summary: ${JSON.stringify(summary)}`);
+  //console.log(`\n\n\n!!!!!!!!!!!!!!!!!!            FINAL RESULT           !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!`);
 }
 // lodBKCxCllyNMHasYrwgOvcmJBPsVkBaQkpRrnOAUcRWPydUBffWKzqrHrcYwnzBFhtvpHgwBHMDtDffPRXFlOYgdLtLQDCjbSwdvIBImYBCsKYfZXZjsisFjVqWYZlC
 
@@ -75,6 +84,36 @@ async function queryBART(data) {
       }
     );
     result = await response.json();
+  }
+  catch (err) {
+    console.error(err);
+  }
+	return result;
+}
+
+
+ function getBase64Decode(urlEncoded) {
+  var result = "";
+  console.log(`${"decoding :" + urlEncoded}`);
+  try{
+    let buff = Buffer.from(urlEncoded, "base64");
+    result = buff.toString('utf8');
+    console.log(`${"result :" + result}`);
+  }
+  catch (err) {
+    console.error(err);
+  }
+	return result;
+}
+
+ function enrichURL(url) {
+  var result = url;
+  try{
+    if(url.slice(-1) == "5")
+    {
+      result = url.slice(0, -1); // remove last char
+    }
+    console.log(`${"result :" + result}`);
   }
   catch (err) {
     console.error(err);
